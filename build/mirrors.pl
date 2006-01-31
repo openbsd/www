f19632c338529e168f84c4d4066f0271cefe5429
steven <steven@openbsd.org>
2006-01-31 21:53:37 +0000

    ignore leading "the" in country name when sorting mirrors. feedback/ok grunk@

--- build/mirrors.pl
+++ build/mirrors.pl
@@ -7,7 +7,7 @@
 use strict;
 use warnings 'all';
 use IO::Handle;		# for $fh->getlines()
-my $RCS_ID = '$OpenBSD: mirrors.pl,v 1.3 2006/01/28 14:14:53 steven Exp $';
+my $RCS_ID = '$OpenBSD: mirrors.pl,v 1.4 2006/01/31 21:53:37 steven Exp $';
 
 my $sources = {
 	'mirrors.dat'	=> 'mirrors.dat',
@@ -164,7 +164,10 @@ sub write_ftphtml($$) {
 
 # helper function to sort entries by country
 sub _by_country {
-	$a->{'GC'} cmp $b->{'GC'}
+	my ($x, $y) = ($a->{'GC'}, $b->{'GC'});
+	$x =~ s/^the\s+//i;	# ignore leading 'the' as in 'The Netherlands'
+	$y =~ s/^the\s+//i;
+	return $x cmp $y;
 }
 
 # main()
