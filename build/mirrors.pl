6adacd97bd13a097eda658d1ac1b0501da693bc9
steven <steven@openbsd.org>
2006-04-26 06:49:33 +0000

    extend the mirror infrastructure to generate anoncvs.html. please enter any new anoncvs mirrors in mirrors.dat from now. ok grunk@

--- build/mirrors.pl
+++ build/mirrors.pl
@@ -7,7 +7,7 @@
 use strict;
 use warnings 'all';
 use IO::Handle;		# for $fh->getlines()
-my $RCS_ID = '$OpenBSD: mirrors.pl,v 1.5 2006/03/02 19:11:50 steven Exp $';
+my $RCS_ID = '$OpenBSD: mirrors.pl,v 1.6 2006/04/26 06:49:33 steven Exp $';
 
 my $sources = {
 	'mirrors.dat'	=> 'mirrors.dat',
@@ -15,10 +15,13 @@ my $sources = {
 	'ftp-mid1'	=> 'mirrors/ftp.html.mid1',
 	'ftp-mid2'	=> 'mirrors/ftp.html.mid2',
 	'ftp-end'	=> 'mirrors/ftp.html.end',
+	'anoncvs-head'	=> 'mirrors/anoncvs.html.head',
+	'anoncvs-end'	=> 'mirrors/anoncvs.html.end'
 };
 my $targets = {
 	'ftplist'	=> '../ftplist',
-	'ftp.html'	=> '../ftp.html'
+	'ftp.html'	=> '../ftp.html',
+	'anoncvs.html'	=> '../anoncvs.html'
 };
 
 # read in mirror list from given file into an array of hash references.
@@ -108,34 +111,62 @@ sub _paste_mirrorlist($$$$) {
 	print $fh ' ' x 4;		# indent for first <td> to come
 	for my $lv (1, 2, 3) {
 	foreach my $mirror (sort _by_country @$mirrorref) {
-		next if (($lv <= 2) && (! defined $mirror->{'LF'}));
-		next if ((defined $mirror->{'LF'}) && ($mirror->{'LF'} != $lv));
-		next unless ($mirror->{$type});
-		# first/second level mirrors have different title
-		my $title;
-		if ((defined $mirror->{'LF'}) && ($mirror->{'LF'} == 1)) {
-			$title = "Master Fanout Site ($mirror->{'GC'})";
-		} elsif ((defined $mirror->{'LF'}) && ($mirror->{'LF'} == 2)) {
-			$title = "Second Level Mirror<br>";
-			$title .= '(' if ($mirror->{'GT'} || $mirror->{'GS'}
-			    || $mirror->{'GC'});
-			$title .= $mirror->{'GT'} if $mirror->{'GT'};
-			$title .= ", $mirror->{'GS'}" if $mirror->{'GS'};
-			$title .= ", $mirror->{'GC'}" if $mirror->{'GC'};
-			$title .= ')' if ($mirror->{'GT'} || $mirror->{'GS'}
-			    || $mirror->{'GC'});
-		} else {
-			$title = "$mirror->{'GC'}";
-			$title .= " ($mirror->{'GT'}" if ($mirror->{'GT'});
-			$title .= ", $mirror->{'GS'}" if ($mirror->{'GS'});
-			$title .= ')' if ($mirror->{'GT'});
+		if ($type eq 'UF' || $type eq 'UH' || $type eq 'UR') {
+			next if (($lv <= 2) && !defined $mirror->{'LF'});
+			next if ((defined $mirror->{'LF'}) && ($mirror->{'LF'} != $lv));
 		}
-		print $fh "<tr>\n	<td>\n	<strong>$title</strong>\n";
-		print $fh "	</td><td>\n";
-		($links) && print $fh "	<a href=\"$mirror->{$type}\">\n";
-		print $fh "	$mirror->{$type}";
-		($links) && print $fh "</a>";
-		print $fh "\n	</td>\n    </tr>";
+		elsif ($type eq 'AH') {
+			next if (($lv <= 2) && !defined $mirror->{'LC'});
+			next if ((defined $mirror->{'LC'}) && ($mirror->{'LC'} != $lv));
+		}
+		next unless ($mirror->{$type});
+		my $loc = _get_location ($type, $mirror);
+		if ($type eq 'UF' || $type eq 'UH' || $type eq 'UR') {
+			print $fh "<tr>\n\t<td>\n\t<strong>$loc</strong>\n";
+			print $fh "\t</td><td>\n";
+			($links) && print $fh "	<a href=\"$mirror->{$type}\">\n";
+			print $fh "\t$mirror->{$type}";
+			($links) && print $fh "</a>";
+			print $fh "\n\t</td>\n    </tr>";
+		}
+		elsif ($type eq 'AH') {
+			if ($mirror->{'AH'} && $mirror->{'AU'} && $mirror->{'AR'}) {
+				print $fh "<li><strong>CVSROOT=",
+					$mirror->{'AU'}, '@',
+					$mirror->{'AH'}, ':',
+					$mirror->{'AR'}, "</strong><br>\n";
+			} else {
+				die "Unable to determine CVSROOT for $mirror->{AH}.\nCheck for missing fields.\n";
+			}
+			if ($mirror->{'HA'}) {
+				print $fh "Host also known as <strong>",
+				join(", ", split(/\s+/, $mirror->{'HA'})),
+				"</strong>.<br>\n";
+			}
+			print $fh "Location: $loc.<br>\n";
+			print $fh "Maintained by <a href=\"mailto:",
+					$mirror->{'ME'}, "\">",
+					$mirror->{'MN'}, "</a>.<br>\n"
+				if ($mirror->{'ME'} && $mirror->{'MN'});
+			print $fh "Protocols: $mirror->{'AP'}.<br>\n"
+				if ($mirror->{'AP'});
+			if ($mirror->{'CE'}) {
+				print $fh "Updated every $mirror->{'CE'} hours";
+				print $fh " from $mirror->{'CF'}"
+					if ($mirror->{'CF'});
+				print $fh ".<br>\n";
+			}
+			print $fh "SSH fingerprints:<br>\n"
+				if ($mirror->{'SD'} || $mirror->{'SR'} ||
+					$mirror->{'SO'});
+			print $fh "(RSA1) $mirror->{'SO'}<br>\n"
+				if ($mirror->{'SO'});
+			print $fh "(RSA) $mirror->{'SR'}<br>\n"
+				if ($mirror->{'SR'});
+			print $fh "(DSA) $mirror->{'SD'}<br>\n"
+				if ($mirror->{'SD'});
+			print $fh "<p>\n";
+		}
 	}
 	}
 	print $fh "\n";
@@ -161,6 +192,17 @@ sub write_ftphtml($$) {
 	close($fh) or die "close $filename: $!";
 }
 
+sub write_anoncvshtml($$) {
+	my ($filename, $mirrorref) = @_;
+
+	open(my $fh, '>', $filename) or die "open $filename: $!";
+	_paste_in($fh, $sources->{'anoncvs-head'});
+	# produce cvsync mirror list
+	_paste_mirrorlist($fh, $mirrorref, 'AH', 1);
+	_paste_in($fh, $sources->{'anoncvs-end'});
+	close($fh) or die "close $filename: $!";
+}
+
 
 # helper function to sort entries by country
 sub _by_country {
@@ -170,12 +212,61 @@ sub _by_country {
 	return lc($x) cmp lc($y);
 }
 
+# helper function to make a location string
+sub _get_location($$) {
+	my $type = shift;
+	my $m = shift;
+
+	my $location = "";
+	if ($type eq 'UF' || $type eq 'UH' || $type eq 'UR') {
+		# first/second level mirrors have different title
+		if ((defined $m->{'LF'}) && ($m->{'LF'} == 1)) {
+			$location = "Master Fanout Site ($m->{'GC'})";
+		} elsif ((defined $m->{'LF'}) && ($m->{'LF'} == 2)) {
+			$location = "Second Level Mirror<br>";
+			$location .= '(' if ($m->{'GT'} || $m->{'GS'} ||
+				$m->{'GC'});
+			$location .= $m->{'GT'} if $m->{'GT'};
+			$location .= ", $m->{'GS'}" if $m->{'GS'};
+			$location .= ", $m->{'GC'}" if $m->{'GC'};
+			$location .= ')' if ($m->{'GT'} || $m->{'GS'} ||
+				$m->{'GC'});
+		} else {
+			$location = "$m->{'GC'}";
+			$location .= " ($m->{'GT'}" if ($m->{'GT'});
+			$location .= ", $m->{'GS'}" if ($m->{'GS'});
+			$location .= ')' if ($m->{'GT'});
+		}
+	}
+	elsif ($type eq 'AH') {
+		$location .= "$m->{'GI'}, " if ($m->{'GI'});
+		$location .= "$m->{'GT'}, " if ($m->{'GT'});
+		$location .= "$m->{'GS'}, " if ($m->{'GS'});
+		$location .= "$m->{'GC'}" if ($m->{'GC'});
+	}
+
+	return $location;
+}
+
 # main()
 my @mirrors = read_mirrors($sources->{'mirrors.dat'});
 
 # XXX write_ftplist() works, but HTML entities have to be converted in
 # output, and grunk has to find a proper way of getting ftplist into the
 # FTP distribution.
-#write_ftplist($targets->{'ftplist'}, \@mirrors);
 
-write_ftphtml($targets->{'ftp.html'}, \@mirrors);
+if (@ARGV == 1) {
+	my $cmd = $ARGV[0];
+
+	if ($cmd eq 'ftplist') {
+		write_ftplist($targets->{'ftplist'}, \@mirrors);
+	} elsif ($cmd eq 'ftp') {
+		write_ftphtml($targets->{'ftp.html'}, \@mirrors);
+	} elsif ($cmd eq 'anoncvs') {
+		write_anoncvshtml($targets->{'anoncvs.html'}, \@mirrors);
+	} else {
+		die "Unknown mirror target.\n"
+	}
+} else {
+	die "Wrong number of arguments.\n"
+}
