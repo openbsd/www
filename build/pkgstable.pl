ac243322204f7d82039bcc112cfcc6f285a1be28
sturm <sturm@openbsd.org>
2007-01-04 22:28:43 +0000

    deal with arch dependent packages minor cleanup

--- build/pkgstable.pl
+++ build/pkgstable.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-# $OpenBSD: pkgstable.pl,v 1.2 2006/11/01 09:47:21 sturm Exp $
+# $OpenBSD: pkgstable.pl,v 1.3 2007/01/04 22:28:43 sturm Exp $
 
 # Public Domain, Nikolay Sturm <sturm@openbsd.org>
 
@@ -10,25 +10,31 @@ die "usage: $0 <OpenBSD release>" unless $ARGV[0];
 my $release = $ARGV[0];
 
 my $packages = "packages-$release";
-open(my $fh, "< $packages") or die "Cannot read $packages: $!";
+open(my $fh, "<", "$packages") or die "Cannot read $packages: $!";
 
 while (<$fh>) {
 	chomp;
 	my ($pkg, $desc) = split / /, $_, 2;
+	my $archs = "amd64 i386";
 
-	printf "<dt>";
+	if (defined $desc and $desc eq "i386") {
+		$archs = $desc;
+		undef $desc;
+	}
+
+	print "<dt>";
 	if (not defined $desc) {
-		printf "<font color=\"#e00000\">$pkg</font>\n";
+		print "<font color=\"#e00000\">$pkg</font>\n";
 		$desc = "Security fix";
 	} else {
-		printf "$pkg\n";
+		print "$pkg\n";
 	}
 
-	printf "<a href=\"ftp://ftp.openbsd.org/pub/OpenBSD/$release/packages/i386/$pkg\">i386</a>\n";
-	if ($release == 4.0) {
-		printf "<a href=\"ftp://ftp.openbsd.org/pub/OpenBSD/$release/packages/amd64/$pkg\">amd64</a>\n";
+	print "<a href=\"ftp://ftp.openbsd.org/pub/OpenBSD/$release/packages/i386/$pkg\">i386</a>\n";
+	if ($release == 4.0 and $archs =~ /amd64/) {
+		print "<a href=\"ftp://ftp.openbsd.org/pub/OpenBSD/$release/packages/amd64/$pkg\">amd64</a>\n";
 	}
-	printf "<dd>$desc\n";
+	print "<dd>$desc\n";
 }
 
 close($fh);
