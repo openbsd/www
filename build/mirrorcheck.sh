410983652a81486477af6376048f405febf2adab
sthen <sthen@openbsd.org>
2023-06-30 06:00:24 +0000

    print the URL used before pronting stderr output from curl/ftp, without this it isn't clear which mirror generated which error text.

--- build/mirrorcheck.sh
+++ build/mirrorcheck.sh
@@ -36,6 +36,7 @@ add_mirror() {
 }
 
 stderr=$(mktemp) || exit 1
+stderr1=$(mktemp) || exit 1
 
 while read -r key value; do
 	case "$key" in
@@ -55,7 +56,13 @@ while (( i < ${#mirrors[@]} )); do
 	for arch in armv7 sparc64; do
 		path="${mirror}snapshots/$arch/SHA256"
 		printf 'Trying %s\n' "$path"
-		http_code=$(curl -A '' --connect-timeout 6 -s -o /dev/null -w '%{http_code}' -- "$path" 2>>"$stderr")
+		http_code=$(curl -A '' --connect-timeout 6 -s -o /dev/null -w '%{http_code}' -- "$path" 2>>"$stderr1")
+		if [[ -n `cat $stderr1` ]]; then
+			echo ${path} >> $stderr
+			cat $stderr1 >> $stderr
+			echo >> $stderr
+			> $stderr1
+		fi
 		[[ "$http_code" == 200 ]] || set -A error_mirrors "${error_mirrors[@]}" "$http_code $path ${maintainers[i]}"
 	done
 	(( i++ ))
@@ -67,7 +74,13 @@ for mirror in "${mirrors[@]}"; do
 	domain="${mirror#http*://}"
 	domain="${domain%%/*}"
 	printf 'Trying %s\n' "$path"
-	timestamp=$(ftp -MV -U '' -w 6 -o - -- "$path" 2>>"$stderr")
+	timestamp=$(ftp -MV -U '' -w 6 -o - -- "$path" 2>>"$stderr1")
+	if [[ -n `cat $stderr1` ]]; then
+		echo ${path} >> $stderr
+		cat $stderr1 >> $stderr
+		echo >> $stderr
+		> $stderr1
+	fi
 	timestamp=$(date -r "$timestamp" '+%Y-%m-%d %H:%M:%S')
 	set -A timestamps "${timestamps[@]}" "$timestamp $domain"
 done
